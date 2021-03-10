b6fe945522f76761a9f437b860ec7fabc2bd19ed
Charles Roth <charlieroth4@icloud.com>
2021-03-10 11:26:12 -0500

    Flow Windows and Triggers
    - Added artificial delay to explore how windows and triggers work in
      Flow

--- lib/airport_flow.ex
+++ lib/airport_flow.ex
@@ -11,9 +11,15 @@ defmodule AirportFlow do
   end
 
   def open_airports() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
     # Flow operations run in GenStage stage processes making them concurrent
     airports_csv()
     |> File.stream!()
+    # Introduce artificial delay to better understand Flow windows and triggers
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
     # Treat the data source as a producer
     |> Flow.from_enumerable()
     # Flow.map/2 and Flow.filter/2 will act as consumer or producer_consumer
@@ -30,20 +36,32 @@ defmodule AirportFlow do
     |> Flow.reject(&(&1.type == "closed"))
     # Add a partition layer to ensure items sharing same :country are sent to
     # same reducer
-    |> Flow.partition(key: {:key, :country})
+    |> Flow.partition(window: window, key: {:key, :country})
     # Flow.group_by/2 is more convenient than Flow.reduce/3 if your only goal
     # is to group elements in the list
     |> Flow.group_by(fn item ->
       item.country
     end)
-    |> Flow.map(fn {country, data} ->
-      {country, Enum.count(data)}
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done -> {events, acc}
+        {:every, 1000} -> {[], acc}
+      end
     end)
+    # |> Flow.map(fn {country, data} ->
+    #   {country, Enum.count(data)}
+    # end)
     # Take only top 10 by count
-    |> Flow.take_sort(10, fn {_, a}, {_, b} ->
-      a > b
-    end)
+    # |> Flow.take_sort(10, fn {_, a}, {_, b} ->
+    #   a > b
+    # end)
     |> Enum.to_list()
-    |> List.flatten()
+
+    # |> List.flatten()
   end
 end
